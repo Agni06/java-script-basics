a6a761e9cd2ab19b69b0af19a5e619480c1a2c96
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-28 20:52:56 +0530

    Create condition.js

---
+++ condition.js
@@ -0,0 +1,11 @@
+let c =Math.random();
+if(c<0.5)
+{
+  console.log("Your number is less than 0.5");
+  console.log(c);
+}
+if(c>=0.5)
+{
+  console.log("Your number is greater than 0.5");
+  console.log(c);
+}
