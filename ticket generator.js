013976da5d54ee2e64387ad4f304458ef3470654
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-28 21:05:18 +0530

    Create ticket generator.js

---
+++ ticket generator.js
@@ -0,0 +1,19 @@
+const age = 3;
+if(age>= 0 and age<= 5)
+{
+  console.log("Free");
+  console.log(age);
+  
+}
+else if(age>=5 and age<=10)
+{
+  console.log("child $10");
+}
+else if(age>=10 and age<=65)
+{
+  console.log("Adult $20");
+}
+else if(age>65)
+{
+  console.log("senior $10");
+}
