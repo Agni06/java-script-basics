b705f8c392c601b39dac9bad29b41e57879bb754
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-28 21:09:43 +0530

    Create conditional2.js

---
+++ conditional2.js
@@ -0,0 +1,20 @@
+let rate = -99;
+if(rate===3)
+{
+  console.log("your super star");
+}
+else if(rate === 2)
+{
+console.log("Meet Expectations");
+  
+}
+else if(rate === 1)
+{
+console.log("Need improvement");
+  
+}
+else
+{
+console.log("Invalid Rating");
+  
+}
