b5094aaf5761391db9da727d16c3f93bf0cfcd90
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-28 21:40:00 +0530

    Create false..js

---
+++ false..js
@@ -0,0 +1,35 @@
+if(0)
+{
+  console.log("true");
+}
+else
+{
+  console.log("false");
+}
+
+if(null)
+{
+  console.log("true");
+}
+else
+{
+  console.log("false");
+}
+
+if(NaN)
+{
+  console.log("true");
+}
+else
+{
+  console.log("false");
+}
+
+if(undefined)
+{
+  console.log("true");
+}
+else
+{
+  console.log("false");
+}
