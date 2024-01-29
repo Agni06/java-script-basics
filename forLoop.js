b8858dd7971332f92c34214bbb4b8a05dcdb9c0c
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-29 20:17:18 +0530

    Create forLoop.js

---
+++ forLoop.js
@@ -0,0 +1,26 @@
+const arr  =[];
+for(let i=0;i<10;i++)
+  {
+arr[i] = i;
+    console.log(arr);
+    
+  }
+
+const animal = ['lion','tiger','bear'];
+for(let i=0;i<animal.length;i++)
+  {
+    console.log(i, animal[i]);
+    
+  }
+for(let i = animal.length-1;i>=0;i--)
+  {
+console.log(i, animal[i]);
+    
+  }
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+for(let i=0;i<people.length;i++)
+  {
+console.log(people[i].toUpperCase());
+    
+  }
