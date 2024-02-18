5043edb51e9f3a1a721ee535d801366ff9041c7b
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-18 22:21:52 +0530

    Update forLoop1.js

--- forLoop1.js
+++ forLoop1.js
@@ -8,3 +8,30 @@ for(let i=0;i<=4;i++)
     }
     
   }
+for(let i=1;i<=10;i++)
+  {
+    console.log('i is ${i}');
+    for(let j=1;i<4;j++)
+      {
+        console.log('    j is ${j}');
+        
+      }
+  }
+
+const seatingChart = [
+[‘Kristen’, ‘Erik’, ‘Antonio’, ’Namita’], [‘Geoffrey’, ‘Jianita’, ‘Jim’, ‘Rum’], [‘Yuma’,  ’Skura’, ‘Jack’, ‘Agni’]
+]
+for(let i =0 ; i<seatingChart.length; i++)
+  {
+    console.log(seatingChart[i]);
+  }
+
+for(let i =0 ; i<seatingChart.length; i++)
+  {
+    let row = seatingChart[i];
+    console.log('row: ${i}');
+    for(let j=0;j<row.length;j++)
+      {
+        console.log(row[j]);
+      }
+  }
