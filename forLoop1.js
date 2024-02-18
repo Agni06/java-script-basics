5ee86beeebe1971b9d6f481b1833478f8b1bc8ef
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-18 22:06:48 +0530

    Create forLoop1.js

---
+++ forLoop1.js
@@ -0,0 +1,10 @@
+let str = "lol";
+for(let i=0;i<=4;i++)
+  {
+    console.log("outer loop :"  ,i);
+    for(int j=0;j<str.length;j++)
+    {
+      console.log("inner loop: ", str[j]);
+    }
+    
+  }
