3e052f5eb35a981bdbc824fc9cb744982a43c82f
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-19 00:27:32 +0530

    Create output3.js

---
+++ output3.js
@@ -0,0 +1,20 @@
+var grade = "D";
+var res;
+switch(grade)
+  {
+    case "A":
+      res+= 10;
+      break;
+    case "B":
+      res+=20;
+      break;
+    case "C":
+      res+=30;
+      break;
+    case "D":
+      res+=40;
+      break;
+      
+  }
+
+document.write(res);  
