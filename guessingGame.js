6b97547d75632ea5716357440f0a5a31083e6941
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-18 23:29:57 +0530

    Create guessingGame.js

---
+++ guessingGame.js
@@ -0,0 +1,7 @@
+let name  = "agni";
+let res  = prompt("Please guess the name");
+while(res!==name)
+  {
+    res  = prompt("Please guess the name");
+  }
+console.log("Congrates you guessed it " + res);
