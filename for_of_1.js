4f87e2ca2c347f78be5c70a7e14758333508d847
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-19 12:51:57 +0530

    Create for_of_1.js

---
+++ for_of_1.js
@@ -0,0 +1,12 @@
+const seatingChart = [
+[‘Kristen’, ‘Erik’, ‘Antonio’, ’Namita’], [‘Geoffrey’, ‘Jianita’, ‘Jim’, ‘Rum’], [‘Yuma’,  ’Skura’, ‘Jack’, ‘Agni’]
+]
+
+for(let row of seatingChart)
+  {
+    for(let col of row)
+      {
+        console.log(col);
+        
+      }
+  }
